6c85e3a72e64d64c32d783b86c389b8177051011
Duri <durid@union.edu>
2014-07-08 20:27:26 -0400

    Initial Commit

---
+++ ULike/VONAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VONAppDelegate.h
+//  ULike
+//
+//  Created by Duri Abdurahman Duri on 7/8/14.
+//  Copyright (c) 2014 Duri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VONAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ULike/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ULike
+//
+//  Created by Duri Abdurahman Duri on 7/8/14.
+//  Copyright (c) 2014 Duri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VONAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VONAppDelegate class]));
+    }
+}
